34d2a33cf47874026d88127b003f035715ab7e3a
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-06-05 23:19:20 +0100

    add user route to retrieve a member from a project

--- back_end/api/v1/views/users.py
+++ back_end/api/v1/views/users.py
@@ -32,3 +32,22 @@ def get_project_members(current_user, project_id):
             'Message': 'Project not found'
         }
         return make_response(jsonify(response)), 404
+@api_blueprint.route('/users/<user_id>/projects/<project_id>', methods=['GET'])
+@user_status
+def get_project_member(current_user, user_id, project_id):
+    """
+    get a particular user from a project
+    """
+    project = storage.get(Project, project_id)
+    if project:
+        all_p_u = project.members
+        for p_u in all_p_u:
+            if p_u.user_id == user_id:
+                return make_response(jsonify(p_u.user.to_dict())), 200
+        else:
+            response = {
+                'Status': 'Fail',
+                'Message': 'Project not found'
+            }
+            return make_response(jsonify(response)), 404
+
