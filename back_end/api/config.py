675a4b89b9b5725e18d1c9d938d017b1aff687a2
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-06-12 18:11:09 +0100

    protect configuration files

---
+++ back_end/api/config.py
@@ -0,0 +1,9 @@
+import os
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'taskpilot0@gmail.com'
+MAIL_PASSWORD = 'otmhovmuptdchmaj'
+ADMINS = ['sammymore50@gmail.com', 'belkid98@gmail.com']
+SECRET_KEY = '\xa3(\x95\xe6\x7f\xb3\xf1\x85o\xc6\xfe\x05\xe55\xfe|d\xf1\xc4\xe0\x1a\xc5[`S\xcd\xe2\x92~k%\xa7'
